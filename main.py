6f2502dc9caf8db1a67e8c3a4645cb73454f8912
Bezrukov47 <bezr47@gmail.com>
2024-12-16 09:07:17 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# ОРГАНИЗАЦИЯ ПРОГРАММ И МЕТОДЫ СТРОК
+my_string = str("Health of World")
+print(len(my_string))
+ # Вывод строки в верхнем регистре
+print(my_string.upper())
+# Вывод строки в нижнем регистре
+print(my_string.lower())
+# Удаление всех пробелов
+my_string_no_spaces = my_string.replace(" ", "")
+print(my_string_no_spaces)
+# Вывод первого символа
+print(my_string[0])
+# Вывод последнего символа
+print(my_string[-1])
